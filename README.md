b1df3043cef21b53b1dc490dd9f4bb137685494e
Lewis Murray <lewismurray@lewiss-mbp.lan>
2023-01-23 21:03:13 +0000

    Upload of finalised files

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # js-code-quiz
+
+## Description 
+
+This repository demonstrates use of javascript to create a quiz and store highscores to local storage.
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+To setup this repository, simply download the files top your local machine and use a code editor, like [VS Code](https://github.com/microsoft/vscode), to open and edit the files. Open the html file in your browser to view the quiz.
+
+
+## Usage 
+
+The images below show examples of the quiz and highscores being saved:
+
+![Intended landing page screenshot](https://github.com/ibanezlewis/password-generator/blob/main/generatedPasswordExample.png)
+
+You can [view the landing page here](https://ibanezlewis.github.io/password-generator/).
+
+## Credits
+
+Skills for Life Bootcamp for providing the materials to worth with.
+
+
+## License
+
+Licensed under [MIT Licence](https://choosealicense.com/licenses/mit/)
+
+---
---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        "title": "Common Data Types DO NOT include:",
+        "choices": ["String", "Booleans", "Alerts", "Numbers"],
+        "answer": "Alerts"
+    },
+    {
+        "title": "Conditional keywords are:",
+        "choices": ["if", "for", "while", "for each"],
+        "answer": "if"
+    },
+    {
+        "title": "Inside which HTML element do we put the JavaScript?",
+        "choices": ["<js>", "<script>", "<scripting>", "<javascript>"],
+        "answer": "<script>"
+    },
+    {
+        "title": "Where is the correct place to insert a JavaScript?",
+        "choices": ["Both the <head> section and the <body> section are correct", "The <body> section", "The <head> section"],
+        "answer": "Both the <head> section and the <body> section are correct"
+    },
+    {
+        "title": "How do you create a function in JavaScript?",
+        "choices": ["function:myFunction()", "function myFunction()", "function = myFunction()"],
+        "answer": "function myFunction()"
+    },
+]
